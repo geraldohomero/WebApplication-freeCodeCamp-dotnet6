766a0f484361a18fe3cd9eca3df0ccad26805ee6
geraldohomero <geraldohomero@protonmail.com>
2023-05-23 18:29:36 -0300

    edit server

---
+++ WebApplication1-dotnet6/Migrations/20230523185338_Migration-NewWindows.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1_dotnet6.Migrations
+{
+    public partial class MigrationNewWindows : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
