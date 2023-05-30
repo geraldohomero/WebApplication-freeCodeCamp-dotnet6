652ac47bc1d8510b7033dc104418ffd5049e4c2f
geraldohomero <geraldohomero@protonmail.com>
2023-05-30 18:18:33 -0300

    add data Annotations for validation

--- WebApplication1-dotnet6/Models/Category.cs
+++ WebApplication1-dotnet6/Models/Category.cs
@@ -9,6 +9,8 @@ namespace WebApplication1_dotnet6.Models
         [Required]
         public string Name { get; set; }
         [Required]
+        [Display(Name ="Display Order")]
+        [Range(1,100,ErrorMessage = "Display Order must be between 1 and 100 only!")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
