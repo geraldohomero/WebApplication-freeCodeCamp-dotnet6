088e31c5ab6ba69765cec9eaf3e14cee6ca50a59
geraldohomero <geraldohomero@protonmail.com>
2023-05-30 18:42:18 -0300

    add Edit funcionality

--- WebApplication1-dotnet6/Controllers/CategoryController.cs
+++ WebApplication1-dotnet6/Controllers/CategoryController.cs
@@ -47,5 +47,52 @@ namespace WebApplication1_dotnet6.Controllers
             }
             return View(obj);
         }
-    }
+   
+
+//GET
+    public IActionResult Edit(int? id)
+    {
+        if(id == null || id == 0)
+        {
+            return NotFound();
+        }
+        var categoryFromDb = _db.Categories.Find(id);
+        //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+        //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+    
+        if(categoryFromDb == null)
+        {
+            return NotFound();
+        }
+    
+        return View(categoryFromDb);
 }
+    
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category obj)
+    {
+        if (obj.DisplayOrder == 0)
+        {
+            ModelState.AddModelError("DisplayOrder", "The Display Order cannot be 0");
+        }
+    
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("DisplayOrder", "The Display Order cannot exactly match the Name");
+            ModelState.AddModelError("name", "The Name cannot exactly match the Display Order");
+        }
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+        }
+
+}
+
+
