07dbb78f3f555b5422c89e194031f055d873790e
Geraldo Homero <geraldohomero@protonmail.com>
2023-07-08 09:12:19 -0300

    Update ApplicationDbContext.cs

--- WebApplication1-dotnet6/Data/ApplicationDbContext.cs
+++ WebApplication1-dotnet6/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using WebApplication1_dotnet6.Models;
 
 namespace WebApplication1_dotnet6.Data
 {
+
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
